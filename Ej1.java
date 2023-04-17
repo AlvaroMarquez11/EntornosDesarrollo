4e96348620fca106b1855aa32d33038db1a807f9
AlvaroMarquez <alvaro.marquez11@educa.madrid.org>
2023-04-17 10:37:17 +0200

    primer commit

---
+++ Ej1.java
@@ -0,0 +1,8 @@
+public class Ej1 {
+
+double potentialEnergy(double mass, double height) {
+
+    return mass * height * 9.81;
+  
+  }
+}
---
+++ PizzaDelivery.java
@@ -0,0 +1,17 @@
+public class PizzaDelivery {
+
+    // ...
+  
+    int getRating() {
+  
+      return moreThanFiveLateDeliveries() ? 2 : 1;
+  
+    }
+  
+    boolean moreThanFiveLateDeliveries() {
+    int numberOfLateDeliveries = 0;
+      return numberOfLateDeliveries > 5;
+  
+    }
+  
+  }
